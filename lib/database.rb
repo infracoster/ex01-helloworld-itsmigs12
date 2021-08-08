a8a55cc235b197daa043966d5ac9db90570f4fb6
infracoster <96405939+infracoster@users.noreply.github.com>
2021-08-08 05:35:37 +0000

    fix broken specs

--- lib/database.rb
+++ lib/database.rb
@@ -6,4 +6,4 @@ require 'byebug'
 ActiveRecord::Schema.verbose = false
 ActiveRecord::Base.establish_connection(adapter: 'sqlite3', database: ':memory:')
 
-ActiveRecord::B
+ActiveRecord::Base.send :include, AttributeN
