be65bd12d7a68f2a64f33b99b81b041ec7cf959d
infracoster <96405939+infracoster@users.noreply.github.com>
2022-01-26 11:15:51 +0000

    minor.

--- lib/main.rb
+++ lib/main.rb
@@ -4,4 +4,4 @@ require 'digest'
 require File.join(ROOT_PATH, '/lib/object')
 require File.join(ROOT_PATH, '/lib/mapping')
 require File.join(ROOT_PATH, '/lib/config')
-require 
+require File.join(ROOT_PAT
