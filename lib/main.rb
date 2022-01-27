92982b57b3312f89af6726583e791b78e67df1ae
infracoster <96405939+infracoster@users.noreply.github.com>
2022-01-27 18:25:09 +0000

    Update working

--- lib/main.rb
+++ lib/main.rb
@@ -5,3 +5,4 @@ require File.join(ROOT_PATH, '/lib/object')
 require File.join(ROOT_PATH, '/lib/mapping')
 require File.join(ROOT_PATH, '/lib/config')
 require File.join(ROOT_PATH, '/lib/utils')
+require File.join(
