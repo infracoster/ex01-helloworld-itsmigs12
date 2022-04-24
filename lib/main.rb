fab8ce26271f790459f9f397b50b62ec2945354c
infracoster <96405939+infracoster@users.noreply.github.com>
2022-04-24 19:30:47 +0000

    POM: maven.imagej.net -> maven.scijava.org

--- lib/main.rb
+++ lib/main.rb
@@ -26,4 +26,4 @@ def retrieve_trades_data
   if $config.read_from_cache && File.exists?($config.cache_data_path)
     data = YAML.load_file($config.cache_data_path)
   else
-    api = Co
+    api = CoinTracking::Api.
