d08a2a885ddcf0959c8a10a48feba5e7b895aabf
infracoster <96405939+infracoster@users.noreply.github.com>
2022-07-10 17:17:59 +0000

    RELEASE:Set next development version of parent pom

--- lib/main.rb
+++ lib/main.rb
@@ -34,4 +34,6 @@ def retrieve_trades_data
   puts "Saving trades to memory..."
   data.each_pair do |id, values|
     next unless id.to_i.to_s == id.to_s
-    t = Trade.from_cointracking(id, 
+    t = Trade.from_cointracking(id, values)
+    t.save!
+    
