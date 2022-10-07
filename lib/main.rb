97355d1b78aa64cdb5d2a69ee6141130d72c3afe
infracoster <96405939+infracoster@users.noreply.github.com>
2022-10-07 13:40:59 +0000

    Address comment

--- lib/main.rb
+++ lib/main.rb
@@ -54,4 +54,5 @@ def trading_line(trade)
   fee_coin    = trade.fee_currency
   buy_coin    = COIN_MAPPING[buy_coin]  || buy_coin
   sell_coin   = COIN_MAPPING[sell_coin] || sell_coin
-  fee_coin    = COIN_MAPPING[fee_coin]  ||
+  fee_coin    = COIN_MAPPING[fee_coin]  || fee_coin
+  buy_a
