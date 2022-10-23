fd89be4328a9abd09f4c405bf02687e755ef5d81
infracoster <96405939+infracoster@users.noreply.github.com>
2022-10-23 14:13:53 +0000

    add .gitignore file

--- lib/main.rb
+++ lib/main.rb
@@ -66,4 +66,5 @@ def trading_line(trade)
   line << eight_decimals(buy_amount)
   line << eight_decimals(sell_amount)
   line << sell_coin
-  line << eight_decimals(t
+  line << eight_decimals(trade.fee_amount)
+  line 
