f9300b8713dda511038174c870a16889cbaface4
infracoster <96405939+infracoster@users.noreply.github.com>
2022-12-31 18:38:57 +0000

    #40

--- lib/main.rb
+++ lib/main.rb
@@ -115,4 +115,6 @@ def write_trade_lines_for_order_hash(order_hash)
 
   if $config.combine_trades?
     grouped_rel = rel.group(:order_hash)
-    template_row = rel.first.du
+    template_row = rel.first.dup
+
+    %i(buy_amount se
