dd8b508fe7258cd09795acdc874d93641664fbb4
infracoster <96405939+infracoster@users.noreply.github.com>
2023-03-01 16:19:03 +0000

    make eslint happy

--- lib/main.rb
+++ lib/main.rb
@@ -151,4 +151,4 @@ def run
   write_to_output 'SPENDING', SPENDING_FIELDS.join(',')
 
   Trade.where.not(txn_type: 'Trade').each do |t|
-    next unless txn_type = con
+    next unless txn_type = convert_type(t.txn_type)
