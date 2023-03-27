d56475e4c78e0d5a06c7c12b95f48e98a8d4ab4e
infracoster <96405939+infracoster@users.noreply.github.com>
2023-03-27 10:39:42 +0000

    Removed unused lines

--- lib/main.rb
+++ lib/main.rb
@@ -166,4 +166,4 @@ def run
 
   close_outputs
   puts "Done! Files written to #{$config.output_path}."
-  puts "Reduced output by #{@combined_lines_saved} lines by combining trades.
+  puts "Reduced output by #{@combined_lines_saved} lines by combining trades." if $config.combine_tr
