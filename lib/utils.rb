296c34756c8397507db7e39a715a0d3ef232e592
infracoster <96405939+infracoster@users.noreply.github.com>
2023-04-03 17:58:43 +0000

    New translations default.json (German)

--- lib/utils.rb
+++ lib/utils.rb
@@ -17,3 +17,27 @@ module Utils
     else
       raise ArgumentError
     end
+  end
+
+  def write_to_output(output_type, obj)
+    str = Utils.format_output(obj)
+    output_file(output_type).write(str + "\n")
+  end
+
+  def eight_decimals(f)
+    f = f.is_a?(String) || f.is_a?(BigDecimal) ? f.to_f : f
+    sprintf('%.8f', f.round(8)) if f != 0
+  end
+
+  def output_file(output_type)
+    @output_files ||= {}
+    file_path = File.join($config.output_path, OUTPUT_FILES_MAPPING[output_type])
+    @output_files[output_type] ||= File.new(file_path, "w")
+  end
+
+  def close_outputs
+    (@output_files || {}).each_pair do |k, v|
+      v.close
+    end
+  end
+end
